96829dc60e70146cfa724a2f7756a54cac5973fc
Gaurav Asawa <gaurav.asawa@issc.unipune.ac.in>
2020-11-26 17:15:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Weatherapp from './components/Weatherapp';
+
+function App() {
+
+  
+
+  return (
+    <div className="app">
+     <Weatherapp />
+    </div>
+  );
+}
+
+export default App; 
